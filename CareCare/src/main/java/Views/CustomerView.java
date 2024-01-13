079c920183792628b3dca4ad1b6a959a29482590
O-Perera <oshaniperera76@gmail.com>
2024-01-13 13:32:09 +0530

    updatedCustomerVeiw

--- CareCare/src/main/java/Views/CustomerView.java
+++ CareCare/src/main/java/Views/CustomerView.java
@@ -1,4 +1,24 @@
 package Views;
 
+import javax.swing.*;
+
 public class CustomerView {
+    private JPanel backPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JRadioButton rbtnSareSparts;
+    private JRadioButton rbtnrepair;
+    private JRadioButton rbtnRepaint;
+    private JButton btnAdd;
+    private JButton btnUpdate;
+    private JButton btnDelete;
+    private JLabel lblEmployee;
+    private JLabel lblCustomer;
+    private JLabel lblEmail;
+    private JLabel lblAdress;
+    private JLabel lblContact;
+    private JLabel lblService;
 }
