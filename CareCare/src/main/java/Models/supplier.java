ecdcaac76b7a1a6b7b5c5d93b92193087424f235
minethz <dmsperera7@gmail.com>
2024-01-13 11:25:25 +0530

    update

--- CareCare/src/main/java/Models/supplier.java
+++ CareCare/src/main/java/Models/supplier.java
@@ -1,11 +1,11 @@
 package Models;
 
 public class supplier {
-      public int supplierID;
-      public String name;
-      public String address;
-      public String email;
-      public int phone_number;
+    private int supplierID;
+    private String name;
+    private String address;
+    private String email;
+    private int phone_number;
 
     public supplier(int supplierID, String name, String address, String email, int phone_number) {
         this.supplierID = supplierID;
@@ -14,10 +14,12 @@ public class supplier {
         this.email = email;
         this.phone_number = phone_number;
     }
-//to add supplier
-    public String toString(){
-        return "supplier {" + "SupplierID= "+supplierID+"/"+"SupplierID= "+supplierID+"/"+"Name= "+name+"/"+"Address= "+address+"/"+"Email= "+email+"/"+"Phone Number= "+phone_number+"/";
+
+    //to add supplier
+    public String toString() {
+        return "supplier {" + "SupplierID= " + supplierID + "/" + "SupplierID= " + supplierID + "/" + "Name= " + name + "/" + "Address= " + address + "/" + "Email= " + email + "/" + "Phone Number= " + phone_number + "/";
 
     }
+}
 
 
