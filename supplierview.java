b70b8d830218bcb6864ba83921ed6bddd3586eb8
Shashira Ravishka <147471428+Shashira2004@users.noreply.github.com>
2024-01-13 13:32:57 +0530

    Add files via upload

---
+++ supplierview.java
@@ -0,0 +1,39 @@
+package Views;
+
+import javax.swing.*;
+
+public class supplierview {
+    private JPanel panel1;
+    private JLabel lblID;
+    private JTextField txtID;
+    private JLabel lblname;
+    private JTextField txtname;
+    private JLabel lblAddress;
+    private JTextField txtAddress;
+    private JLabel lblEmail;
+    private JLabel iblphonenumber;
+    private JTextField txtEmail;
+    private JTextField txtPhonenumber;
+    private JLabel lbladd;
+    private JButton btnadd;
+    private JLabel lblupdate;
+    private JButton btnupdate;
+    private JLabel lblIDU;
+    private JTextField textField2;
+    private JLabel lblnameU;
+    private JLabel lblAddressU;
+    private JLabel lblemailU;
+    private JLabel lblPhonenumberU;
+    private JTextField textField1;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JLabel lbldelete;
+    private JButton button1;
+    private JLabel lblIDD;
+    private JTextField textField6;
+
+    public static void main(String[] args) {
+
+    }
+}
