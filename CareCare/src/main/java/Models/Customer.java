508ac8fe058f9df2f7e54b08ca248bd39294d155
O-Perera <oshaniperera76@gmail.com>
2024-01-13 17:30:07 +0530

    customerController

--- CareCare/src/main/java/Models/Customer.java
+++ CareCare/src/main/java/Models/Customer.java
@@ -1,6 +1,68 @@
 package Models;
 
 public class Customer {
+    private int empId;
+    private String name;
+    private String email;
+    private String address;
+    private int contact;
+    private String service;
 
+    public Customer(int empId, String name, String email, String address, int contact, String service) {
+        this.empId = empId;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.contact = contact;
+        this.service = service;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getContact() {
+        return contact;
+    }
+
+    public void setContact(int contact) {
+        this.contact = contact;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
 }
 
