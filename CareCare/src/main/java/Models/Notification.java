cc780114f4d0ebbb91a62e1cb89c6a382765f8d2
DinuRanathunga <151540182+DinuRanathunga@users.noreply.github.com>
2024-01-13 16:48:57 +0530

    Changed

--- CareCare/src/main/java/Models/Notification.java
+++ CareCare/src/main/java/Models/Notification.java
@@ -1,4 +1,16 @@
 package Models;
 
 public class Notification {
+    private int  CustomerId;
+    private int OrderId;
+    private String email;
+    private boolean status;
+
+    public Notification(int CustomerId, int OrderId, String email, boolean status){
+        this.CustomerId = CustomerId;
+        this.email = email;
+        this.OrderId = OrderId;
+        this.status = status;
+    }
+
 }
