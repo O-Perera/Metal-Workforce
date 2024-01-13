b3a4c4f154279053aab20f3cf0039708c6b79174
Hiruni12345 <hirunidinoshi@gmail.com>
2024-01-13 13:12:18 +0530

    updated controller

--- CareCare/src/main/java/Controllers/suppliercontrol.java
+++ CareCare/src/main/java/Controllers/suppliercontrol.java
@@ -4,5 +4,6 @@ public class suppliercontrol {
     public supplier addsupplier(int supplierID, String name, String address, String email, int phone_number) {
         supplier supplierobj = new supplier( supplierID, name,  address,  email, phone_number);
         return supplierobj;
+        
     }
 }
