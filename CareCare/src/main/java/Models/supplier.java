c67ea7fc56e3907ee44c2d6593a4b6bc1822ee38
minethz <dmsperera7@gmail.com>
2024-01-13 10:51:06 +0530

    supplier update

--- CareCare/src/main/java/Models/supplier.java
+++ CareCare/src/main/java/Models/supplier.java
@@ -1,5 +1,17 @@
 package Models;
 
 public class supplier {
-    
+   private int SupplierID;
+   private String Name;
+
+   private String address;
+
+
+   supplier(int SupplierID, String Name, String address)
+   {
+       this.SupplierID = SupplierID;
+       this.Name = Name;
+       this.address = address;
+   }
+
 }
---
+++ CareCare/src/main/java/Views/supplierView.java
@@ -0,0 +1,10 @@
+package Views;
+
+import javax.swing.*;
+
+public class supplierView {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+}
