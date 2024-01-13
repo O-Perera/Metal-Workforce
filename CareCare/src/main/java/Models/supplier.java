31ffca3847903e71b8e93eececee7425476eff69
minethz <dmsperera7@gmail.com>
2024-01-13 10:58:18 +0530

    dlt

--- CareCare/src/main/java/Models/supplier.java
+++ CareCare/src/main/java/Models/supplier.java
@@ -1,17 +1,6 @@
 package Models;
 
 public class supplier {
-   private int SupplierID;
-   private String Name;
-
-   private String address;
-
-
-   supplier(int SupplierID, String Name, String address)
-   {
-       this.SupplierID = SupplierID;
-       this.Name = Name;
-       this.address = address;
-   }
+   
 
 }
